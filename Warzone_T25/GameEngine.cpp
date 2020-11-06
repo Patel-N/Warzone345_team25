bcb99ce01292d32f5e63adb158761e9a04bba8f2
Mohamed-Yasser Houssein <mike.012555@gmail.com>
2020-11-06 10:45:23 -0500

    game engine creation

---
+++ Warzone_T25/GameEngine.cpp
@@ -0,0 +1,14 @@
+#include "GameEngine.h"
+#include<iostream>
+
+using namespace std;
+
+GameEngine::GameEngine(){
+	cout << "Game Engine Created";
+}
+
+void GameEngine::reinforcementPhase(){}
+
+void GameEngine::issueOrdersPhase(){}
+
+void GameEngine::executeOrdersPhase(){}
