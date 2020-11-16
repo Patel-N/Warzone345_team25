6a29bd362536f2e973756e33558bc68c1a475be2
Mohamed-Yasser Houssein <mike.012555@gmail.com>
2020-11-16 11:37:21 -0500

    cleaned warzone driver

--- Warzone_T25/Warzone_T25.cpp
+++ Warzone_T25/Warzone_T25.cpp
@@ -8,43 +8,8 @@
 #include"GameEngine.h"
 int main()
 {
-	MapLoader* loader = new MapLoader();
-	Map* map;
-	map = new Map(loader->generateMap("./MapFiles/canada.map"));
-	cout << *map;
-	GameEngine* engine = new GameEngine(map);
-	Player* player1 = new Player(1,"Yasser",50);
-	map->assignOccupantToTerritory(1, player1);
-	Territory* territory = map->getTerritory(1);
-	Territory* territory2 = map->getTerritory(2);
-	territory->setNumArmies(20);
-	player1->assignTerritoryToPlayer(territory);
-	cout << *player1;
-	//part_1::start();  
-	//part_2::start();
-	//part_3::start();
-	//part_4::start();
-	//part_5::start();
 
+	part_4::start();
 
 }
-
-/*
-	Player:
-		Go through everything with Martin
-		Outputstream needs to be updated
-
-
-	Order:
-		Check abstract implementation and see how it would work for issuing Orders
-
-	Map:
-		Create method to assigning player* to a territory
-
-	MapLoader:
-		InputStream
-
-	Cards:
-		Make sure everything is good
-*/
 
