d141f77fdeb2deeab6adeb75d1bdcd4396ca5f9d
Martin M <m.marcos25@hotmail.com>
2020-12-03 19:21:53 -0500

    updated human strategy
    round robin upgrade in progress

--- Warzone_T25/Warzone_T25.cpp
+++ Warzone_T25/Warzone_T25.cpp
@@ -29,6 +29,7 @@ int main()
 		card->set_type(rand() % 5 + 1);
 		deck->addCardToDeck(card);
 	}
+
 	Player* player1 = new Player(1, "Neil", 1, 1);
 	Player* player2 = new Player(2, "Martin", 2, 1);
 	Player* player3 = new Player(3, "Paurush", 3, 1);
@@ -38,10 +39,16 @@ int main()
 	Hand* hand_p3 = new Hand();
 	Hand* hand_p4 = new Hand();
 	//setting player's hand
+
+
 	player1->setPlayerHand(hand_p1);
 	player2->setPlayerHand(hand_p2);
 	player3->setPlayerHand(hand_p3);
 	player4->setPlayerHand(hand_p4);
+
+	Card* card1 = new Card(1);
+	player1->getPlayerHand()->addCardToHand(card1);
+
 	engine->addPlayer(player1);
 	engine->addPlayer(player2);
 	engine->addPlayer(player3);
